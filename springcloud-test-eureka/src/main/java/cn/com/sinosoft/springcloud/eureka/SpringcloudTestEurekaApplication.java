7f682f71f0d752a00cc5d1dcdc995568e469be72
ylj_com <youlijie@sinosoft.com.cn>
2018-01-15 20:33:31 +0800

    Initial commit

---
+++ springcloud-test-eureka/src/main/java/cn/com/sinosoft/springcloud/eureka/SpringcloudTestEurekaApplication.java
@@ -0,0 +1,12 @@
+package cn.com.sinosoft.springcloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudTestEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudTestEurekaApplication.class, args);
+	}
+}
